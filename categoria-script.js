c2400b57940ebb30f72311c1fe9314fe6367e4ca
IsaqueBRL <isaquemoises14@gmail.com>
2025-09-02 11:25:46 -0400

    1

--- categoria-script.js
+++ categoria-script.js
@@ -192,4 +192,4 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         document.getElementById('categoria-titulo').textContent = 'Categoria não especificada';
     }
-}); 
+});
